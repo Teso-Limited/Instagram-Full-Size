34dab4e404a25a94d49df096f30269e37a7a17c2
TesoMayn <tesomayn@gmail.com>
2015-12-16 13:08:16 -0500

    Fixed auto-sizing

--- Instagram Full Size.user.js
+++ Instagram Full Size.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Instagram Full Size
 // @namespace    https://tesomayn.com
-// @version      2.2.4
+// @version      2.2.5
 // @description  CTRL click an Instagram image to open it's full size
 // @author       TesoMayn
 // @match        *://*.instagram.com/*
@@ -20,6 +20,7 @@ $(document).ready( function() {
 
                 if( full.indexOf('s640x640') !== -1 ) { full = full.replace('/s640x640', ''); }
                 if( full.indexOf('p750x750') !== -1 ) { full = full.replace('/p750x750', ''); }
+                if( full.indexOf('s750x750') !== -1 ) { full = full.replace('/s750x750', ''); }
 
                 if(e.ctrlKey) { e.preventDefault(); window.open(full, '_blank'); console.log(full); }
             });
@@ -28,3 +29,4 @@ $(document).ready( function() {
     makeLarge();
     $(window).scroll(function () { makeLarge(); });
 });
+
