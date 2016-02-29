110ff63a0257b27e20b32306c55b67c06481eec3
Teso Mayn <TesoMayn@users.noreply.github.com>
2016-02-29 01:20:41 -0500

    Update Instagram Full Size.user.js

--- Instagram Full Size.user.js
+++ Instagram Full Size.user.js
@@ -1,23 +1,18 @@
 // ==UserScript==
 // @name         Instagram Full Size
 // @namespace    https://tesomayn.com
-// @version      2.3.0
-// @description  Alt-click an Instagram image to open it's full size
+// @version      2.3.1
+// @description  Alt-click an Instagram image or video to open it's full size
 // @author       TesoMayn
 // @match        *://*.instagram.com/*
 // @grant        unsafeWindow
-// @grant        GM_registerMenuCommand
-// @grant        GM_openInTab
 // ==/UserScript==
 
 $(document).ready( function(x) {
     function makeLarge() {
-        $('#react-root > section > main > article > div > div > div > a, #react-root > section > main > section > div > div > article > div').each(function(m) {
-            $(this).mousedown(function(e) {
-                var full = $(this).find('img').attr('src');
-                full = full.replace(/e35\/.*\//, '');
-                full = full.replace(/[p|s][0-9]\w+/g, '');
-
+        $('#react-root > section > main > article > div > div > div > a, #react-root > section > main > section > div > div > article > div').each( function(m) {
+            $(this).mousedown( function(e) {
+                var full = $(this).find('img').attr('src').replace(/e35\/.*\//, '').replace(/[p|s][0-9]\w+/g, '');
                 if( e.altKey ) { window.open(full, '_blank'); console.log(full); }
             });
         });
