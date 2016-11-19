d24c0b4137213ba074c21f1e5f8f1667f1c2a058
Teso Mayn <TesoMayn@users.noreply.github.com>
2016-11-19 03:20:22 -0500

    Update 4.0.0
    Added video support.
    Renamed to "Instagram Tweaks" to get ready for major updates and new features

--- Instagram Full Size.user.js
+++ Instagram Full Size.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         Instagram Full Size
+// @name         Instagram Tweaks
 // @namespace    https://tesomayn.com
-// @version      3.0.0
-// @description  Middle Mouse Click Any Image To Open Full Size
+// @version      4.0.0
+// @description  Middle-click any image (posted or default picture) to open full size; Click video thumbnail then middle-click ANYWHERE to view .mp4
 // @author       TesoMayn
 // @match        *://*.instagram.com/*
 // @grant        unsafeWindow
@@ -25,6 +25,17 @@ $(document).ready( function() {
             }
             e.preventDefault();
         });
+
+        if ($('video').length) {
+            $("body").on('mousedown', function(e) {
+                if( (e.which == 2) ) {
+                    var src = $('video', this).attr('src');
+                    window.open(src, '_blank');
+                    console.log(src);
+                }
+                e.preventDefault();
+            });
+        }
     });
     console.log('Instagram Full-Size v'+GM_info.script.version+' Initialized');
 });
